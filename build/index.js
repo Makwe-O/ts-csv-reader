cf95c2059f2b4af3a1b29606189c992fe5c748e4
onyeka mmakwe <notmaks@yahoo.com>
2020-02-19 20:22:25 +0100

    uses of enums

--- build/index.js
+++ build/index.js
@@ -1,10 +1,27 @@
 "use strict";
-var Csv = /** @class */ (function () {
-    function Csv(file) {
-        this.file = file;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+var content = fs_1.default
+    .readFileSync('football.csv', {
+    encoding: 'utf-8'
+})
+    .split('\n')
+    .map(function (row) { return row.split(','); });
+var count = 0;
+var MatchResults;
+(function (MatchResults) {
+    MatchResults["HomeWin"] = "H";
+    MatchResults["AwayWin"] = "A";
+    MatchResults["Draw"] = "D";
+})(MatchResults || (MatchResults = {}));
+for (var _i = 0, content_1 = content; _i < content_1.length; _i++) {
+    var match = content_1[_i];
+    if ((match[1] === 'Man United' && match[5] === MatchResults.HomeWin) ||
+        (match[2] === 'Man United' && match[5] === MatchResults.AwayWin)) {
+        count++;
     }
-    Csv.prototype.call = function () {
-        console.log('Hi');
-    };
-    return Csv;
-}());
+}
+console.log('Manchester wins: ', count);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
-class Csv {
-  constructor(public file: String) {}
+import fs from 'fs';
 
-  call(): void {
-    console.log('Hi');
+const content = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8'
+  })
+  .split('\n')
+  .map(row => row.split(','));
+
+let count = 0;
+
+enum MatchResults{
+  HomeWin = 'H';
+  AwayWin = 'A',
+  Draw = 'D'
+}
+
+for (let match of content) {
+  if (
+    (match[1] === 'Man United' && match[5] === MatchResults.HomeWin) ||
+    (match[2] === 'Man United' && match[5] === MatchResults.AwayWin)
+  ) {
+    count++;
   }
 }
+console.log('Manchester wins: ', count);
