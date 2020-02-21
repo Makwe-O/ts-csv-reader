12a7480fb01f242612e31af6e0b0eb14679656c4
onyeka mmakwe <notmaks@yahoo.com>
2020-02-22 00:03:28 +0100

    remove build

--- build/utils/parser.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dateStringToDate = function (data) {
-    var splitDate = data.split('/').map(function (val) { return parseInt(val); });
-    return new Date(splitDate[2], splitDate[1] - 1, splitDate[0]);
-};
